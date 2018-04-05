79087d4773aed8ee302fa94064d96a61bea1e45d
Renato Iwashima <renatoiwa@gmail.com>
2018-04-05 15:43:16 -0700

    solved custom actions as recorded

--- app/src/main/java/com/linkedin/android/learning_android_accessibility/adapters/CardListAdapter.java
+++ app/src/main/java/com/linkedin/android/learning_android_accessibility/adapters/CardListAdapter.java
@@ -3,8 +3,12 @@ package com.linkedin.android.learning_android_accessibility.adapters;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Color;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.view.AccessibilityDelegateCompat;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.view.accessibility.AccessibilityNodeInfoCompat;
 import android.support.v7.widget.RecyclerView;
 import android.text.format.DateUtils;
 import android.view.LayoutInflater;
@@ -19,6 +23,8 @@ import com.linkedin.android.learning_android_accessibility.models.CardItem;
 
 import java.util.Calendar;
 import java.util.List;
+
+import static android.support.v4.view.accessibility.AccessibilityNodeInfoCompat.*;
 
 public class CardListAdapter extends RecyclerView.Adapter<CardListAdapter.ViewHolder> {
 
@@ -44,7 +50,7 @@ public class CardListAdapter extends RecyclerView.Adapter<CardListAdapter.ViewHo
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
+    public void onBindViewHolder(@NonNull final ViewHolder holder, final int position) {
         Context context = holder.itemView.getContext();
         CardItem item = getItem(position);
 
@@ -62,15 +68,15 @@ public class CardListAdapter extends RecyclerView.Adapter<CardListAdapter.ViewHo
         holder.mImage.setImageResource(item.getImageId());
 
         // content description for image buttons
-        Resources res = context.getResources();
+        final Resources res = context.getResources();
         String name = item.getName();
         String moreOptionsDescription = String.format(res.getString(R.string.cards_card_more_options_button), name);
-        String commentDescription = String.format(res.getString(R.string.cards_card_comment_button), name);
-        String shareDescription = String.format(res.getString(R.string.cards_card_share_button), name);
+        final String commentDescription = String.format(res.getString(R.string.cards_card_comment_button), name);
+        final String shareDescription = String.format(res.getString(R.string.cards_card_share_button), name);
 
         // content description for these buttons change based on state
-        String likeDescription;
-        String favoriteDescription;
+        final String likeDescription;
+        final String favoriteDescription;
 
         if (item.isLiked()) {
             likeDescription = String.format(res.getString(R.string.cards_card_unlike_button), name);
@@ -93,11 +99,56 @@ public class CardListAdapter extends RecyclerView.Adapter<CardListAdapter.ViewHo
         }
 
         // assigning all content descriptions
-        holder.mMoreOptionsButton.setContentDescription(moreOptionsDescription);
-        holder.mLikeButton.setContentDescription(likeDescription);
-        holder.mCommentButton.setContentDescription(commentDescription);
-        holder.mFavoriteButton.setContentDescription(favoriteDescription);
-        holder.mShareButton.setContentDescription(shareDescription);
+//        holder.mMoreOptionsButton.setContentDescription(moreOptionsDescription);
+//        holder.mLikeButton.setContentDescription(likeDescription);
+//        holder.mCommentButton.setContentDescription(commentDescription);
+//        holder.mFavoriteButton.setContentDescription(favoriteDescription);
+//        holder.mShareButton.setContentDescription(shareDescription);
+
+        ViewCompat.setAccessibilityDelegate(holder.itemView, new AccessibilityDelegateCompat() {
+            @Override
+            public void onInitializeAccessibilityNodeInfo(View host, AccessibilityNodeInfoCompat info) {
+                super.onInitializeAccessibilityNodeInfo(host, info);
+
+                info.addAction(new AccessibilityActionCompat(AccessibilityActionCompat.ACTION_CLICK.getId(), "View full post"));
+
+                info.addAction(new AccessibilityActionCompat(R.id.action_card_like, likeDescription));
+                info.addAction(new AccessibilityActionCompat(R.id.action_card_comment, commentDescription));
+                info.addAction(new AccessibilityActionCompat(R.id.action_card_favorite, favoriteDescription));
+                info.addAction(new AccessibilityActionCompat(R.id.action_card_share, shareDescription));
+                info.addAction(new AccessibilityActionCompat(R.id.action_card_archive, res.getString(R.string.cards_card_archive_this_post)));
+                info.addAction(new AccessibilityActionCompat(R.id.action_card_remove, res.getString(R.string.cards_card_remove_this_post)));
+                info.addAction(new AccessibilityActionCompat(R.id.action_card_report, res.getString(R.string.cards_card_report_this_post)));
+            }
+
+            @Override
+            public boolean performAccessibilityAction(View host, int action, Bundle args) {
+                switch (action) {
+                    case R.id.action_card_like:
+                        holder.onClick(holder.mLikeButton);
+                        return true;
+                    case R.id.action_card_comment:
+                        holder.onClick(holder.mCommentButton);
+                        return true;
+                    case R.id.action_card_favorite:
+                        holder.onClick(holder.mFavoriteButton);
+                        return true;
+                    case R.id.action_card_share:
+                        holder.onClick(holder.mShareButton);
+                        return true;
+                    case R.id.action_card_archive:
+                        removeItem(position);
+                        return true;
+                    case R.id.action_card_remove:
+                        removeItem(position);
+                        return true;
+                    case R.id.action_card_report:
+                        removeItem(position);
+                        return true;
+                }
+                return super.performAccessibilityAction(host, action, args);
+            }
+        });
     }
 
     @Override
@@ -154,6 +205,21 @@ public class CardListAdapter extends RecyclerView.Adapter<CardListAdapter.ViewHo
             mFavoriteButton = itemView.findViewById(R.id.cards_card_favorite);
             mShareButton = itemView.findViewById(R.id.cards_card_share);
             mMoreOptionsButton = itemView.findViewById(R.id.cards_card_more_options);
+
+            ViewCompat.setImportantForAccessibility(mLikeButton, ViewCompat.IMPORTANT_FOR_ACCESSIBILITY_NO);
+            mLikeButton.setContentDescription(null);
+
+            ViewCompat.setImportantForAccessibility(mCommentButton, ViewCompat.IMPORTANT_FOR_ACCESSIBILITY_NO);
+            mCommentButton.setContentDescription(null);
+
+            ViewCompat.setImportantForAccessibility(mFavoriteButton, ViewCompat.IMPORTANT_FOR_ACCESSIBILITY_NO);
+            mFavoriteButton.setContentDescription(null);
+
+            ViewCompat.setImportantForAccessibility(mShareButton, ViewCompat.IMPORTANT_FOR_ACCESSIBILITY_NO);
+            mShareButton.setContentDescription(null);
+
+            ViewCompat.setImportantForAccessibility(mMoreOptionsButton, ViewCompat.IMPORTANT_FOR_ACCESSIBILITY_NO);
+            mMoreOptionsButton.setContentDescription(null);
 
             mLikeButton.setOnClickListener(this);
             mCommentButton.setOnClickListener(this);
